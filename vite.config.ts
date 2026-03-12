299d26dba5d51c3a859e93dce945794cabf7245c
Niftalem Awel <niftalemrex@gmail.com>
2026-03-12 09:21:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Detect if we are in production (GH Pages) or local dev
-const isGHPages = process.env.NODE_ENV === 'production'
+// Detect environment
+const isGHPages = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   plugins: [react()],
   base: isGHPages
-    ? '/WEMS-Wedding-Events-Management-System/' // GitHub Pages path
-
-})
+    ? "/WEMS-Wedding-Events-Management-System/"
+    : "/",
+});
